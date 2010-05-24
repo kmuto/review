a0a4e3bf15971dc5a28ae3a0662c2041327f490c
takahashim <maki@rubycolor.org>
2010-05-24 17:43:49 +0900

    added more headline tests

--- test/test_epubbuilder.rb
+++ test/test_epubbuilder.rb
@@ -8,7 +8,7 @@ class EPUBBuidlerTest < Test::Unit::TestCase
 
   def setup
     @builder = EPUBBuilder.new()
-    param = {
+    @param = {
       "secnolevel" => 2,    # for IDGXMLBuilder, EPUBBuilder
       "inencoding" => "UTF-8",
       "outencoding" => "UTF-8",
@@ -16,16 +16,38 @@ class EPUBBuidlerTest < Test::Unit::TestCase
       "stylesheet" => nil,  # for EPUBBuilder
     }
     compiler = ReVIEW::Compiler.new(@builder)
-    compiler.setParameter(param)
+    compiler.setParameter(@param)
     chapter = Chapter.new(nil, 1, '-', nil, StringIO.new)
-    chapter.setParameter(param)
+    chapter.setParameter(@param)
     location = Location.new(nil, nil)
     @builder.bind(compiler, chapter, location)
   end
 
-  def test_headline
+  def test_headline_level1
     @builder.headline(1,"test","this is test.")
     assert_equal %Q|<h1 id='test'>第1章　this is test.</h1>\n|, @builder.raw_result
   end
 
+  def test_headline_level1_without_secno
+    @param["secnolevel"] = 0
+    @builder.headline(1,"test","this is test.")
+    assert_equal %Q|<h1 id='test'>this is test.</h1>\n|, @builder.raw_result
+  end
+
+  def test_headline_level2
+    @builder.headline(2,"test","this is test.")
+    assert_equal %Q|\n<h2 id='test'>1.1　this is test.</h2>\n|, @builder.raw_result
+  end
+
+  def test_headline_level3
+    @builder.headline(3,"test","this is test.")
+    assert_equal %Q|\n<h3 id='test'>this is test.</h3>\n|, @builder.raw_result
+  end
+
+
+  def test_headline_level3_with_secno
+    @param["secnolevel"] = 3
+    @builder.headline(3,"test","this is test.")
+    assert_equal %Q|\n<h3 id='test'>1.0.1　this is test.</h3>\n|, @builder.raw_result
+  end
 end
