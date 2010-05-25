35734be0e25ebdf14d017556ccc7ebae1461158b
takahashim <maki@rubycolor.org>
2010-05-25 17:39:18 +0900

    fixed tests for epubbuilder

--- test/test_epubbuilder.rb
+++ test/test_epubbuilder.rb
@@ -25,29 +25,29 @@ class EPUBBuidlerTest < Test::Unit::TestCase
 
   def test_headline_level1
     @builder.headline(1,"test","this is test.")
-    assert_equal %Q|<h1 id='test'>第1章　this is test.</h1>\n|, @builder.raw_result
+    assert_equal %Q|<a name="h1" /><h1 id='test'>第1章　this is test.</h1>\n|, @builder.raw_result
   end
 
   def test_headline_level1_without_secno
     @param["secnolevel"] = 0
     @builder.headline(1,"test","this is test.")
-    assert_equal %Q|<h1 id='test'>this is test.</h1>\n|, @builder.raw_result
+    assert_equal %Q|<a name="h1" /><h1 id='test'>this is test.</h1>\n|, @builder.raw_result
   end
 
   def test_headline_level2
     @builder.headline(2,"test","this is test.")
-    assert_equal %Q|\n<h2 id='test'>1.1　this is test.</h2>\n|, @builder.raw_result
+    assert_equal %Q|\n<a name="h1-1" /><h2 id='test'>1.1　this is test.</h2>\n|, @builder.raw_result
   end
 
   def test_headline_level3
     @builder.headline(3,"test","this is test.")
-    assert_equal %Q|\n<h3 id='test'>this is test.</h3>\n|, @builder.raw_result
+    assert_equal %Q|\n<a name="h1-0-1" /><h3 id='test'>this is test.</h3>\n|, @builder.raw_result
   end
 
 
   def test_headline_level3_with_secno
     @param["secnolevel"] = 3
     @builder.headline(3,"test","this is test.")
-    assert_equal %Q|\n<h3 id='test'>1.0.1　this is test.</h3>\n|, @builder.raw_result
+    assert_equal %Q|\n<a name="h1-0-1" /><h3 id='test'>1.0.1　this is test.</h3>\n|, @builder.raw_result
   end
 end
