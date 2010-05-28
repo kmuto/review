7c1e7096f7b4418eb9ea104a65b58c8a79fb6351
takahashim <maki@rubycolor.org>
2010-05-28 15:00:41 +0900

    fix invalid URL(in namespace)

--- lib/review/epubbuilder.rb
+++ lib/review/epubbuilder.rb
@@ -62,7 +62,7 @@ module ReVIEW
         header = <<EOT
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:ops=" http://www.idpf.org/2007/ops" xml:lang="ja" lang="ja">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:ops="http://www.idpf.org/2007/ops" xml:lang="ja" lang="ja">
 <head>
   <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
   <meta http-equiv="Content-Style-Type" content="text/css"/>
