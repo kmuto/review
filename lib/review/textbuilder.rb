dd1a8a38e372880a18e27e0659a5bf0f9729e729
Kenshi Muto <kmuto@debian.org>
2010-12-04 00:22:07 +0900

    * doc/quickstart.rdoc: created. * lib/review/textbuilder.rb: provide a wrapper of topbuilder.rb.   'review-compile --target text' (or review2text) makes a plain   text file with pseudo tags.

---
+++ lib/review/textbuilder.rb
@@ -0,0 +1,17 @@
+# textbuilder.rb
+#
+# Copyright (c) 2010 Kenshi Muto
+#
+# This program is free software.
+# You can distribute or modify this program under the terms of
+# the GNU LGPL, Lesser General Public License version 2.1.
+#
+
+require 'review/topbuilder'
+
+module ReVIEW
+
+  class TEXTBuilder < TOPBuilder
+  end
+
+end   # module ReVIEW
