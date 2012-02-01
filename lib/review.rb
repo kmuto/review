f506d725342de4303f490b0c21a975510f1c3580
kdmsnr <kdmsnr@gmail.com>
2012-02-01 16:41:55 +0900

    add default_external: refs #75

--- lib/review.rb
+++ lib/review.rb
@@ -1,3 +1,6 @@
+if defined?(Encoding) && Encoding.respond_to?("default_external")
+  Encoding.default_external = "UTF-8"
+end
 require 'review/i18n'
 require 'review/exception'
 require 'review/compiler'
