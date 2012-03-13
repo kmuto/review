0c979b0a5437bdeed93feed3e91595d54d33f60a
kdmsnr <kdmsnr@gmail.com>
2012-03-13 13:58:27 +0900

    Split tests from test_book.rb

---
+++ test/test_book_chapter.rb
@@ -0,0 +1,409 @@
+# -*- coding: utf-8 -*-
+require 'book_test_helper'
+class ChapterTest < Test::Unit::TestCase
+  include BookTestHelper
+
+  def setup
+
+    if "".respond_to?(:encode)
+      @utf8_str = "あいうえお"
+      @eucjp_str = "あいうえお".encode("EUC-JP")
+      @sjis_str = "あいうえお".encode("Shift_JIS")
+      @jis_str = "あいうえお".encode("ISO-2022-JP")
+    else
+      @utf8_str = "\xe3\x81\x82\xe3\x81\x84\xe3\x81\x86\xe3\x81\x88\xe3\x81\x8a" # "あいうえお"
+      @eucjp_str = "\xa4\xa2\xa4\xa4\xa4\xa6\xa4\xa8\xa4\xaa"
+      @sjis_str = "\x82\xa0\x82\xa2\x82\xa4\x82\xa6\x82\xa8"
+      @jis_str = "\x1b\x24\x42\x24\x22\x24\x24\x24\x26\x24\x28\x24\x2a\x1b\x28\x42"
+    end
+=begin
+    @utf8_str = "\xe3\x81\x82\xe3\x81\x84\xe3\x81\x86\xe3\x81\x88\xe3\x81\x8a" # "あいうえお"
+    @eucjp_str = "\xa4\xa2\xa4\xa4\xa4\xa6\xa4\xa8\xa4\xaa"
+    @sjis_str = "\x82\xa0\x82\xa2\x82\xa4\x82\xa6\x82\xa8"
+    @jis_str = "\x1b\x24\x42\x24\x22\x24\x24\x24\x26\x24\x28\x24\x2a\x1b\x28\x42"
+=end
+  end
+
+  def test_s_intern_pathes
+    dir1_files = {
+      'CHAPS' => "ch1.re\nch2.re\n",
+      'ch1.re' => 'ch1',
+      'ch2.re' => 'ch2',
+    }
+    dir2_files = {
+      'CHAPS' => "ch1.re\n",
+      'ch1.re' => 'ch1',
+    }
+    mktmpbookdir dir1_files do |dir1, book1, files1|
+      mktmpbookdir dir2_files do |dir2, book2, files2|
+        paths = (files1.values + files2.values).flatten.grep(/\.re\z/)
+        chs = Book::Chapter.intern_pathes(paths)
+
+        assert_equal 3, chs.size
+        assert chs[0].book == chs[1].book
+        assert chs[0].book != chs[2].book
+        assert dir1, chs[0].book.basedir
+        assert dir2, chs[2].book.basedir
+      end
+    end
+
+    dir_files = {
+      'CHAPS' => "ch1.re\n",
+      'ch1.re' => 'ch1',
+    }
+    mktmpbookdir dir_files do |dir, book, files|
+      paths = files.values.grep(/\.re\z/)
+      paths << __FILE__ + ' not exist file.re'
+      assert_raises IndexError, KeyError do
+        Book::Chapter.intern_pathes(paths)
+      end
+    end
+
+    dir_files = {
+      'ch1.re' => 'ch1 not in CHAPS',
+      'ch2.re' => 'ch2 not in CHAPS',
+    }
+    mktmpbookdir dir_files do |dir, book, files|
+      paths = files.values.grep(/\.re\z/)
+      assert_nothing_raised do
+        Book::Chapter.intern_pathes(paths)
+      end
+    end
+
+    dir_files = {
+      'CHAPS' => "ch1.re\n",
+      'ch1.re' => 'ch1',
+      'ch2.re' => 'ch2 not in CHAPS',
+    }
+    mktmpbookdir dir_files do |dir, book, files|
+      paths = files.values.grep(/\.re\z/)
+      assert_raises IndexError, KeyError do
+        Book::Chapter.intern_pathes(paths)
+      end
+    end
+
+    dir_files = {
+      'CHAPS' => "ch1.re\n",
+      'ch1.re' => 'ch1',
+      'preface.re' => 'preface',
+    }
+    mktmpbookdir dir_files do |dir, book, files|
+      paths = files.values.grep(/\.re\z/)
+      assert_nothing_raised do
+        Book::Chapter.intern_pathes(paths)
+      end
+    end
+  end
+
+  def test_s_for_stdin
+    assert Book::Chapter.for_stdin
+  end
+
+  def test_s_for_path
+    assert Book::Chapter.for_path(1, __FILE__)
+  end
+
+  def test_initialize
+    ch = Book::Chapter.new(:book, :number, :name, '/foo/bar', :io)
+    assert_equal :book, ch.env
+    assert_equal :book, ch.book
+    assert_equal :number, ch.number
+    assert_equal '/foo/bar', ch.path
+    assert_equal "#<ReVIEW::Book::Chapter number /foo/bar>", ch.inspect
+  end
+
+  def test_dirname_and_basename
+    ch = Book::Chapter.new(nil, nil, nil, nil, nil)
+    assert_equal nil, ch.dirname
+    assert_equal nil, ch.basename
+
+    ch = Book::Chapter.new(nil, nil, nil, '/foo/bar', nil)
+    assert_equal '/foo', ch.dirname
+    assert_equal 'bar', ch.basename
+
+    ch = Book::Chapter.new(nil, nil, nil, 'bar', nil)
+    assert_equal '.', ch.dirname
+    assert_equal 'bar', ch.basename
+  end
+
+  def test_name
+    ch = Book::Chapter.new(nil, nil, 'foo', nil)
+    assert_equal 'foo', ch.name
+
+    ch = Book::Chapter.new(nil, nil, 'foo.bar', nil)
+    assert_equal 'foo', ch.name
+
+    ch = Book::Chapter.new(nil, nil, nil, nil)
+    assert_raises(TypeError) { ch.name } # XXX: OK?
+  end
+
+  def test_open
+    ch = Book::Chapter.new(nil, nil, nil, __FILE__, :io)
+    assert_equal :io, ch.open
+    assert_equal [:io], ch.open {|io| [io] }
+
+    ch = Book::Chapter.new(nil, nil, nil, __FILE__)
+    assert_equal __FILE__, ch.open.path
+    assert_equal [__FILE__], ch.open {|io| [io.path] }
+  end
+
+  def test_size
+    ch = Book::Chapter.new(nil, nil, nil, __FILE__, :io)
+    assert_equal File.size(__FILE__), ch.size
+
+    File.open(__FILE__, 'r') do |i|
+      ch = Book::Chapter.new(nil, nil, nil, nil, i)
+      assert_raises(TypeError) do # XXX: OK?
+        ch.size
+      end
+    end
+  end
+
+  def test_title
+    io = StringIO.new
+    ch = Book::Chapter.new(nil, nil, nil, nil, io)
+    assert_equal '', ch.title
+
+    io = StringIO.new("=1\n=2\n")
+    ch = Book::Chapter.new(nil, nil, nil, nil, io)
+    assert_equal '1', ch.title
+
+
+    [
+      ['EUC', @eucjp_str],
+      ['SJIS', @sjis_str],
+#      ['JIS', @jis_str],
+      ['XYZ', @eucjp_str],
+    ].each do |enc, instr|
+      io = StringIO.new("= #{instr}\n")
+      ch = Book::Chapter.new(nil, nil, nil, nil, io)
+      ReVIEW.book.param = {'inencoding' => enc}
+      assert_equal @utf8_str, ch.title
+      assert_equal @utf8_str, ch.instance_eval { @title }
+    end
+  end
+
+  def test_content
+    [
+      ['EUC', @eucjp_str],
+      ['SJIS', @sjis_str],
+      ['JIS', @jis_str],
+      ['XYZ', @eucjp_str],
+    ].each do |enc, instr|
+      tf = Tempfile.new('chapter_test')
+      begin
+        tf.print instr
+        tf.close
+
+        ch = Book::Chapter.new(nil, nil, nil, tf.path)
+        ReVIEW.book.param = {'inencoding' => enc}
+        assert_equal @utf8_str, ch.content
+        assert_equal @utf8_str, ch.instance_eval { @content }
+      ensure
+        tf.close(true)
+      end
+
+      tf1 = Tempfile.new('chapter_test1')
+      tf2 = Tempfile.new('chapter_test2')
+      begin
+        tf1.puts instr
+        tf1.puts instr
+        tf1.close
+        tf2.puts instr
+        tf1.close
+
+        ch = Book::Chapter.new(nil, nil, nil, tf1.path, tf2)
+        ReVIEW.book.param = {'inencoding' => enc}
+        assert_equal "#{@utf8_str}\n#{@utf8_str}\n", ch.content # XXX: OK?
+      ensure
+        tf1.close(true)
+        tf2.close(true)
+      end
+    end
+  end
+
+  def test_lines
+    lines = ["1\n", "2\n", "3"]
+    tf = Tempfile.new('chapter_test')
+    tf.print lines.join('')
+    tf.close
+
+    ch = Book::Chapter.new(nil, nil, nil, tf.path)
+    assert_equal lines, ch.lines
+
+    lines = ["1\n", "2\n", "3"]
+    tf1 = Tempfile.new('chapter_test1')
+    tf1.print lines.join('')
+    tf1.close
+    tf2 = Tempfile.new('chapter_test2')
+    tf2.puts lines.join('')
+    tf2.puts lines.join('')
+    tf2.close
+
+    ch = Book::Chapter.new(nil, nil, nil, tf1.path, tf2.path)
+    assert_equal lines, ch.lines # XXX: OK?
+  end
+
+  def test_volume
+    content = "abc\ndef"
+    tf1 = Tempfile.new('chapter_test1')
+    tf1.print content
+    tf1.close
+    tf2 = Tempfile.new('chapter_test2')
+    tf2.print content
+    tf2.print content
+    tf2.close
+
+    ch = Book::Chapter.new(nil, nil, nil, tf1.path)
+    assert ch.volume
+    assert_equal content.gsub(/\s/, '').size, ch.volume.bytes
+
+    ch = Book::Chapter.new(nil, nil, nil, tf1.path, tf2)
+    assert ch.volume
+    assert_equal content.gsub(/\s/, '').size, ch.volume.bytes # XXX: OK?
+  end
+
+  def test_on_CHAPS?
+    mktmpbookdir 'CHAPS' => "chapter1.re\nchapter2.re",
+        'chapter1.re' => '12345', 'preface.re' => 'abcde' do |dir, book, files|
+      ch1 = Book::Chapter.new(book, 1, 'chapter1', files['chapter1.re'])
+      pre = Book::Chapter.new(book, nil, 'preface', files['preface.re'])
+
+      assert ch1.on_CHAPS?
+      assert !pre.on_CHAPS?
+
+      ch2_path = File.join(dir, 'chapter2.er')
+      File.open(ch2_path, 'w') {}
+      ch2 = Book::Chapter.new(book, 2, 'chapter2', ch2_path)
+
+      ch3_path = File.join(dir, 'chapter3.er')
+      File.open(ch3_path, 'w') {}
+      ch3 = Book::Chapter.new(book, 3, 'chapter3', ch3_path)
+
+      assert ch2.on_CHAPS?
+      assert !ch3.on_CHAPS?
+    end
+  end
+
+  def test_list_index
+    do_test_index(<<E, Book::ListIndex, :list_index, :list)
+//list
+//listnum [abc]
+//list [def]
+//table [def]
+//table [others]
+E
+  end
+
+  def test_table_index
+    do_test_index(<<E, Book::TableIndex, :table_index, :table)
+//table
+//table [abc]
+//table [def]
+//list [def]
+//list [others]
+E
+  end
+
+  def test_footnote_index
+    content = <<E
+//footnote
+//footnote [abc][text...]
+//footnote [def][text...]
+//footnote [xyz]
+//list [def]
+//list [others]
+E
+    do_test_index(content, Book::FootnoteIndex, :footnote_index, :footnote) do |ch|
+      assert_raises IndexError,KeyError do
+        ch.footnote('xyz')
+      end
+    end
+  end
+
+  def test_bibpaper
+    do_test_index(<<E, Book::BibpaperIndex, :bibpaper_index, :bibpaper, :filename => 'bib.re')
+//bibpaper
+//bibpaper [abc][text...]
+//bibpaper [def][text...]
+//bibpaper [xyz]
+//list [def]
+//list [others]
+E
+    assert_raises FileNotFound do
+      do_test_index('', Book::BibpaperIndex, :bibpaper_index, :bibpaper, :filename => 'bib')
+    end
+  end
+
+  def test_headline_index
+    do_test_index(<<E, Book::HeadlineIndex, :headline_index, :headline, :propagate => false)
+==
+== abc
+== def
+=== def
+//table others
+E
+  end
+
+  def test_image
+    do_test_index(<<E, Book::ImageIndex, :image_index, :image)
+//image
+//image [abc]
+//image [def]
+//list [def]
+//list [others]
+E
+
+    do_test_index(<<E, Book::NumberlessImageIndex, :numberless_image_index, :image, :propagate => false)
+//numberlessimage
+//numberlessimage [abc]
+//numberlessimage [def]
+//list [def]
+//list [others]
+E
+
+    do_test_index(<<E, Book::ImageIndex, :image_index, :image)
+//image
+//numberlessimage [abc]
+//image [def]
+//list [def]
+//list [others]
+E
+
+    do_test_index(<<E, Book::NumberlessImageIndex, :numberless_image_index, :image, :propagate => false)
+//image
+//numberlessimage [abc]
+//image [def]
+//list [def]
+//list [others]
+E
+  end
+
+  def do_test_index(content, klass, list_method, ref_method, opts = {})
+    Dir.mktmpdir do |dir|
+      path = File.join(dir, opts[:filename] || 'chapter.re')
+
+      book = Book::Base.new(dir)
+
+      ch = nil
+      File.open(path, 'w') do |o|
+        o.print content
+        ch = Book::Chapter.new(book, 1, 'chapter', o.path)
+      end
+
+      assert ch.__send__(ref_method, 'abc')
+      assert ch.__send__(ref_method, 'def')
+      assert_raises IndexError,KeyError do
+        ch.__send__(ref_method, nil)
+      end
+      assert_raises IndexError,KeyError do
+        ch.__send__(ref_method, 'others')
+      end
+      assert_raises IndexError,KeyError do
+        ch.__send__(ref_method, 'not exist id')
+      end
+
+      yield(ch) if block_given?
+    end
+  end
+end
---
+++ test/test_book_part.rb
@@ -0,0 +1,50 @@
+require 'book_test_helper'
+class PartTest < Test::Unit::TestCase
+  include BookTestHelper
+
+  def test_initialize
+    part = Book::Part.new(nil, nil)
+    assert_equal nil, part.number
+    assert_equal nil, part.chapters
+    assert_equal '', part.name
+
+    part = Book::Part.new(123, [], 'name')
+    assert_equal 123, part.number
+    assert_equal [], part.chapters
+    assert_equal 'name', part.name
+  end
+
+  def test_each_chapter
+    part = Book::Part.new(nil, [1, 2, 3])
+
+    tmp = []
+    part.each_chapter do |ch|
+      tmp << ch
+    end
+    assert_equal [1, 2, 3], tmp
+  end
+
+  def test_volume
+    part = Book::Part.new(nil, [])
+    assert part.volume
+    assert_equal 0, part.volume.bytes
+    assert_equal 0, part.volume.chars
+    assert_equal 0, part.volume.lines
+
+    chs = []
+    Tempfile.open('part_test') do |o|
+      o.print "12345"
+      chs << Book::Chapter.new(nil, nil, nil, o.path)
+    end
+    Tempfile.open('part_test') do |o|
+      o.print "67890"
+      chs << Book::Chapter.new(nil, nil, nil, o.path)
+    end
+
+    part = Book::Part.new(nil, chs)
+    assert part.volume
+    assert part.volume.bytes > 0
+    assert part.volume.chars > 0
+    assert part.volume.lines > 0
+  end
+end
