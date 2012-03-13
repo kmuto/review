c2a9afd6d75232dbe5b1e5e9ee220c94ad9e1a40
kdmsnr <kdmsnr@gmail.com>
2012-03-13 13:53:00 +0900

    Extract book_test_helper

--- test/test_book.rb
+++ test/test_book.rb
@@ -1,39 +1,6 @@
 # encoding: utf-8
 
-require 'test_helper'
-require 'review/book'
-
-require 'stringio'
-require 'tempfile'
-require 'tmpdir'
-
-include ReVIEW
-module BookTestHelper
-  def mktmpbookdir(files = {})
-    created_files = {}
-    Dir.mktmpdir do |dir|
-      files.each_pair do |basename, content|
-        path = File.join(dir, basename)
-        File.open(path, 'w') {|o| o.print content }
-        created_files[basename] = path
-      end
-      book = Book::Base.new(dir)
-      yield(dir, book, created_files)
-    end
-  end
-
-  def get_instance_variables(obj)
-    obj.instance_variables.inject({}) do |memo, name|
-      value = obj.instance_variable_get(name)
-      if value.instance_variables.empty?
-        memo[name] = value
-      else
-        memo[name] = get_instance_variables(value)
-      end
-      memo
-    end
-  end
-end
+require 'book_test_helper'
 
 class BookTest < Test::Unit::TestCase
   include BookTestHelper
