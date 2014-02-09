269b5533fa6692f400ec084e1bea69bb38ea6a2b
zunda <zundan@gmail.com>
2014-02-08 23:07:26 -1000

    Convert command line option into style of Pygments
    Conversion from the command line/YAML option syntax-highlight to
    Pygments style is moved from bin/review-compile to lib/review/highlighter.rb

--- lib/review/highlighter.rb
+++ lib/review/highlighter.rb
@@ -1,7 +1,27 @@
 module ReVIEW
   module Highlighter
+    def Highlighter::highlighter_opts(engine = :pygments, style = none)
+      case engine
+      when :pygments
+        case style
+        when nil, 'none'
+          return nil
+        when 'monochrome'
+          return {:pygments_opts => {:style =>'bw'}}
+        when 'color'
+          return {:pygments_opts => {:style => 'default'}}
+        else
+          $stderr.puts "syntax-highlight must be none, monochrome, or color"
+          exit 1
+        end
+      else
+        $stderr.puts "highlight engine #{engine} is not supported"
+        exit 1
+      end
+    end
+
     def highlight(ops)
-      if @highlighter_opts[:pygments_opts]
+      if @highlighter_opts and @highlighter_opts[:pygments_opts]
         return highlight_with_pygments(ops)
       end
       body = ops[:body] || ''
