6f054aaa2824aef606b7dfd70025a26483387c2f
takahashim <maki@rubycolor.org>
2014-03-01 11:56:49 +0900

    MARKDOWNBuilder: add @<em>{} (same as @<i>) and @<strong>{} (same as @<b>)

---
+++ test/test_markdownbuilder.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+
+require 'test_helper'
+require 'review/compiler'
+require 'review/book'
+require 'review/htmlbuilder'
+require 'review/i18n'
+
+class MARKDOWNBuilderTest < Test::Unit::TestCase
+  include ReVIEW
+
+  def setup
+    @builder = MARKDOWNBuilder.new()
+    @param = {
+      "secnolevel" => 2,    # for IDGXMLBuilder, HTMLBuilder
+      "inencoding" => "UTF-8",
+      "outencoding" => "UTF-8",
+      "subdirmode" => nil,
+      "stylesheet" => nil,  # for HTMLBuilder
+    }
+    ReVIEW.book.param = @param
+    @compiler = ReVIEW::Compiler.new(@builder)
+    @chapter = Book::Chapter.new(Book::Base.new(nil), 1, '-', nil, StringIO.new)
+    location = Location.new(nil, nil)
+    @builder.bind(@compiler, @chapter, location)
+  end
+
+  def test_inline_em
+    assert_equal "test*foo*abc", @builder.compile_inline("test@<em>{foo}abc")
+  end
+
+  def test_inline_strong
+    assert_equal "test**foo**abc", @builder.compile_inline("test@<strong>{foo}abc")
+  end
+
+end
