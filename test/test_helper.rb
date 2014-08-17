dac334227e52847b3aa59ceef17f6751429276f7
takahashim <maki@rubycolor.org>
2014-08-17 23:31:52 +0900

    remove ol_helper and ul_helper; use compile_block

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,27 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib/')
 require 'test/unit'
 
-def ul_helper(src, expect)
-  io = StringIO.new(src)
-  li = LineInput.new(io)
-  @compiler.__send__(:compile_ulist, li)
-  assert_equal expect, @builder.raw_result
-end
-
-def ol_helper(src, expect)
-  io = StringIO.new(src)
-  li = LineInput.new(io)
-  @compiler.__send__(:compile_olist, li)
-  assert_equal expect, @builder.raw_result
-end
-
-def builder_helper(src, expect, method_sym)
-  io = StringIO.new(src)
-  li = LineInput.new(io)
-  @compiler.__send__(method_sym, li)
-  assert_equal expect, @builder.raw_result
-end
-
 def touch_file(path)
   File.open(path, "w").close
   path
