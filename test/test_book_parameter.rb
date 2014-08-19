1a628b569f82d6f4a79e47530e8ecc3f748b7835
takahashim <maki@rubycolor.org>
2014-08-20 00:59:17 +0900

    add test

--- test/test_book_parameter.rb
+++ test/test_book_parameter.rb
@@ -19,6 +19,15 @@ class ParametersTest < Test::Unit::TestCase
   #   end
   # end
 
+   def test_param_path
+     params = Book::Parameters.new
+     assert_equal '/CHAPS', params.chapter_file # XXX: OK? (leading / and uninitialized @basedir)
+     assert_equal '/PART', params.part_file
+     assert_equal '.re', params.ext
+     assert_equal '/images', params.image_dir
+     assert_equal %w(.ai .psd .eps .pdf .tif .tiff .png .bmp .jpg .jpeg .gif .svg), params.image_types
+   end
+
   def test_s_get_page_metric
     mod = Module.new
     assert_nothing_raised do # XXX: OK?
