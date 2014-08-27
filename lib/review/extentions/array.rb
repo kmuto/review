dcc83143c13b4e2f3c024f557b6ea3fc6df394d2
takahashim <maki@rubycolor.org>
2014-08-27 21:18:41 +0900

    add compat method of Enumerable#map in 1.8

--- lib/review/extentions/array.rb
+++ lib/review/extentions/array.rb
@@ -11,4 +11,15 @@ class Array
   def to_raw
     self.map(&:to_raw).join("")
   end
+
+  if [].map.kind_of?(Array)
+    # Ruby 1.8
+    def map(&block)
+      if !block_given?
+        return to_enum :map
+      else
+        collect(&block)  ## XXX same as original
+      end
+    end
+  end
 end
