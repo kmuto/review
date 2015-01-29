27ce50cefc8930d09f42addae7eee231c403de6f
Masanori Kado <kdmsnr@gmail.com>
2015-01-30 00:46:16 +0900

    Add test

--- test/test_index.rb
+++ test/test_index.rb
@@ -102,5 +102,21 @@ class IndexTest < Test::Unit::TestCase
     assert_equal [2,2], index['sec1-2|sec1-2-2'].number
     assert_equal "1.2.2", index.number('sec1-2|sec1-2-2')
   end
+
+  def test_HeadelineIndex5
+    src = <<-EOB
+= chap1
+====[column] c1
+== sec1-1
+== sec1-2
+=== sec1-2-1
+=== sec1-2-2
+    EOB
+    book = Book::Base.load
+    chap = Book::Chapter.new(book, 1, '-', nil) # dummy
+    index = Book::HeadlineIndex.parse(src, chap)
+    assert_equal [2,2], index['sec1-2-2'].number
+    assert_equal "1.2.2", index.number('sec1-2-2')
+  end
 end
 
