6f2496c0b5674033ad7e02fdad1e691e4fea713a
takahashim <maki@rubycolor.org>
2016-03-06 23:32:32 +0900

    add a test of HTMLBuilder#inline_hd

--- test/test_htmlbuilder.rb
+++ test/test_htmlbuilder.rb
@@ -1059,4 +1059,19 @@ EOS
 EOS
     assert_equal expected, actual
   end
+
+  def test_inline_hd
+    book = ReVIEW::Book::Base.load
+    book.catalog = ReVIEW::Catalog.new({"CHAPS"=>%w(ch1.re ch2.re)})
+    io1 = StringIO.new("= test1\n\nfoo\n\n== test1-1\n\nbar\n\n== test1-2\n\nbar\n\n")
+    io2 = StringIO.new("= test2\n\nfoo\n\n== test2-1\n\nbar\n\n== test2-2\n\nbar\n\n")
+    chap1 = ReVIEW::Book::Chapter.new(book, 1, 'ch1', 'ch1.re', io1)
+    chap2 = ReVIEW::Book::Chapter.new(book, 2, 'ch2', 'ch2.re', io2)
+    book.parts = [ReVIEW::Book::Part.new(self, nil, [chap1, chap2])]
+    builder = ReVIEW::HTMLBuilder.new
+    comp = ReVIEW::Compiler.new(builder)
+    builder.bind(comp, chap2, nil)
+    hd = builder.inline_hd("ch1|test1-1")
+    assert_equal "「1.1 test1-1」", hd
+  end
 end
