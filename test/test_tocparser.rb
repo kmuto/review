f860c043f3f0c3c019cdcdb19b6f6944a27a4c92
takahashim <maki@rubycolor.org>
2016-03-20 10:27:46 +0900

    TOCParser: fix test of counting chapters

--- test/test_tocparser.rb
+++ test/test_tocparser.rb
@@ -16,7 +16,7 @@ class TOCParserTest < Test::Unit::TestCase
     io = StringIO.new("= test\n\naaa\n//image[foo][bar]{\n//}\n\n== test2\n\n=== test3\n\n==test21\n\n=test11\n")
     chap = ReVIEW::Book::Chapter.new(dummy_book, 1, 'foo', "bar/foo.re", io)
     ret = TOCParser.parse(chap)
-    assert_equal 1, ret.size
+    assert_equal 2, ret.size ## XXX how to count chapters including multiple L1 headlines ??
     chap_node = ret[0]
     assert_equal ReVIEW::TOCParser::Chapter, chap_node.class
     assert_equal "foo", chap_node.chapter_id
