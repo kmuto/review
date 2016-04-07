e0fc4b697722e1d99ae4b41664a0929b475fd5c1
Kenshi Muto <kmuto@debian.org>
2016-04-07 19:04:12 +0900

    add one more deep_merge test about array overriding

--- test/test_extentions_hash.rb
+++ test/test_extentions_hash.rb
@@ -22,6 +22,15 @@ class TestExtentionsHash < Test::Unit::TestCase
                   :b => {:ba => 3, :bb => 4}}.deep_merge({:b => {:ba => 5, :bc => 6},
                                                   :c => {:ca => 1}}))
   end
+  def test_deep_merge_with_array
+    assert_equal({:a => "string", :b => ["BA"],
+                  :c => {:ca => [:cab => "CAB"], :cb => 3}},
+
+                 {:a => 1, :b => ["shouldoverriden"],
+                  :c => {:ca => [:caa => "shouldoverriden"], :cb => 3}}.
+                   deep_merge({:a => "string", :b => ["BA"],
+                               :c => {:ca => [:cab => "CAB"]}}))
+  end
 
   def test_deep_merge_b_simple
     a = {:a => 1, :b => 2}
