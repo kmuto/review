f1c65ebc84af40ffeed87fea7b83a65ad12f4b07
Kenshi Muto <kmuto@debian.org>
2016-04-12 21:55:46 +0900

    modify for test

--- test/test_latexbuilder.rb
+++ test/test_latexbuilder.rb
@@ -209,8 +209,10 @@ class LATEXBuidlerTest < Test::Unit::TestCase
   end
 
   def test_jis_x_0201_kana
+    # uplatex can handle half-width kana natively
     actual = compile_inline("foo･ｶﾝｼﾞ､テスト")
-    assert_equal %Q|foo\\aj半角{・}\\aj半角{カ}\\aj半角{ン}\\aj半角{シ}\\aj半角{゛}\\aj半角{、}テスト|, actual
+    assert_equal %Q|foo･ｶﾝｼﾞ､テスト|, actual
+    # assert_equal %Q|foo\\aj半角{・}\\aj半角{カ}\\aj半角{ン}\\aj半角{シ}\\aj半角{゛}\\aj半角{、}テスト|, actual
   end
 
   def test_dlist
