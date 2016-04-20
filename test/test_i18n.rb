878b9e68f7d8124f9351ee777cba8a4e881dce16
takahashim <maki@rubycolor.org>
2016-04-20 15:12:57 +0900

    remove code for 1.8 in test (for Travis)

--- test/test_i18n.rb
+++ test/test_i18n.rb
@@ -6,151 +6,149 @@ require 'tmpdir'
 class I18nTest < Test::Unit::TestCase
   include ReVIEW
 
-  if RUBY_VERSION !~ /^1.8/ ## to avoid Travis error :-(
-    def test_load_locale_yml
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "locale.yml")
-          File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
+  def test_load_locale_yml
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "locale.yml")
+        File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
+        I18n.setup
+        assert_equal "bar", I18n.t("foo")
+      end
+    end
+  end
+
+  def test_load_locale_yaml
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "locale.yaml")
+        File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
+        assert_raise ReVIEW::ConfigError do
           I18n.setup
-          assert_equal "bar", I18n.t("foo")
         end
       end
     end
+  end
 
-    def test_load_locale_yaml
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "locale.yaml")
-          File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
-          assert_raise ReVIEW::ConfigError do
-            I18n.setup
-          end
-        end
+  def test_load_foo_yaml
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "foo.yml")
+        File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
+        I18n.setup("ja","foo.yml")
+        assert_equal "bar", I18n.t("foo")
       end
     end
+  end
 
-    def test_load_foo_yaml
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "foo.yml")
-          File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
-          I18n.setup("ja","foo.yml")
-          assert_equal "bar", I18n.t("foo")
-        end
+  def test_update_foo_yaml
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "foo.yml")
+        File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
+        i18n = ReVIEW::I18n.new("ja")
+        i18n.update_localefile(File.join(Dir.pwd, "foo.yml"))
+        assert_equal "bar", i18n.t("foo")
       end
     end
+  end
 
-    def test_update_foo_yaml
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "foo.yml")
-          File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
-          i18n = ReVIEW::I18n.new("ja")
-          i18n.update_localefile(File.join(Dir.pwd, "foo.yml"))
-          assert_equal "bar", i18n.t("foo")
-        end
+  def test_update_foo_yaml_i18nclass
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "foo.yml")
+        File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
+        I18n.setup("ja", "foo.yml")
+        assert_equal "bar", I18n.t("foo")
       end
     end
+  end
 
-    def test_update_foo_yaml_i18nclass
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "foo.yml")
-          File.open(file, "w"){|f| f.write("locale: ja\nfoo: \"bar\"\n")}
-          I18n.setup("ja", "foo.yml")
-          assert_equal "bar", I18n.t("foo")
-        end
+  def test_load_locale_yml_i18n
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "locale.yml")
+        File.open(file, "w"){|f| f.write("ja:\n  foo: \"bar\"\nen:\n  foo: \"buz\"\n")}
+        I18n.setup
+        assert_equal "bar", I18n.t("foo")
+        assert_equal "図", I18n.t("image")
+        I18n.setup("en")
+        assert_equal "buz", I18n.t("foo")
+        assert_equal "Figure ", I18n.t("image")
       end
     end
+  end
 
-    def test_load_locale_yml_i18n
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "locale.yml")
-          File.open(file, "w"){|f| f.write("ja:\n  foo: \"bar\"\nen:\n  foo: \"buz\"\n")}
+  def test_load_locale_invalid_yml
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "locale.yml")
+        File.open(file, "w"){|f| f.write("local: ja\nfoo: \"bar\"\n")}
+        assert_raises(ReVIEW::KeyError) do
           I18n.setup
-          assert_equal "bar", I18n.t("foo")
-          assert_equal "図", I18n.t("image")
-          I18n.setup("en")
-          assert_equal "buz", I18n.t("foo")
-          assert_equal "Figure ", I18n.t("image")
         end
       end
     end
+  end
 
-    def test_load_locale_invalid_yml
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "locale.yml")
-          File.open(file, "w"){|f| f.write("local: ja\nfoo: \"bar\"\n")}
-          assert_raises(ReVIEW::KeyError) do
-            I18n.setup
-          end
-        end
-      end
-    end
+  def test_custom_format
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        file = File.join(dir, "locale.yml")
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pa章")}
+        I18n.setup("ja")
+        assert_equal "第a章", I18n.t("chapter", 1)
 
-    def test_custom_format
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          file = File.join(dir, "locale.yml")
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pa章")}
-          I18n.setup("ja")
-          assert_equal "第a章", I18n.t("chapter", 1)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pA章")}
+        I18n.setup("ja")
+        assert_equal "第B章", I18n.t("chapter", 2)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pA章")}
-          I18n.setup("ja")
-          assert_equal "第B章", I18n.t("chapter", 2)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pAW章")}
+        I18n.setup("ja")
+        assert_equal "第Ｂ章", I18n.t("chapter", 2)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pAW章")}
-          I18n.setup("ja")
-          assert_equal "第Ｂ章", I18n.t("chapter", 2)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%paW章")}
+        I18n.setup("ja")
+        assert_equal "第ｂ章", I18n.t("chapter", 2)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%paW章")}
-          I18n.setup("ja")
-          assert_equal "第ｂ章", I18n.t("chapter", 2)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pR章")}
+        I18n.setup("ja")
+        assert_equal "第I章", I18n.t("chapter", 1)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pR章")}
-          I18n.setup("ja")
-          assert_equal "第I章", I18n.t("chapter", 1)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pr章")}
+        I18n.setup("ja")
+        assert_equal "第ii章", I18n.t("chapter", 2)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pr章")}
-          I18n.setup("ja")
-          assert_equal "第ii章", I18n.t("chapter", 2)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pRW章")}
+        I18n.setup("ja")
+        assert_equal "第Ⅻ章", I18n.t("chapter", 12)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pRW章")}
-          I18n.setup("ja")
-          assert_equal "第Ⅻ章", I18n.t("chapter", 12)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pJ章")}
+        I18n.setup("ja")
+        assert_equal "第二十七章", I18n.t("chapter", 27)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pJ章")}
-          I18n.setup("ja")
-          assert_equal "第二十七章", I18n.t("chapter", 27)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pdW章")}
+        I18n.setup("ja")
+        assert_equal "第１章", I18n.t("chapter", 1)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pdW章")}
-          I18n.setup("ja")
-          assert_equal "第１章", I18n.t("chapter", 1)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pdW章")}
+        I18n.setup("ja")
+        assert_equal "第27章", I18n.t("chapter", 27)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pdW章")}
-          I18n.setup("ja")
-          assert_equal "第27章", I18n.t("chapter", 27)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pDW章")}
+        I18n.setup("ja")
+        assert_equal "第１章", I18n.t("chapter", 1)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pDW章")}
-          I18n.setup("ja")
-          assert_equal "第１章", I18n.t("chapter", 1)
+        File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pDW章")}
+        I18n.setup("ja")
+        assert_equal "第２７章", I18n.t("chapter", 27)
 
-          File.open(file, "w"){|f| f.write("locale: ja\nchapter: 第%pDW章")}
-          I18n.setup("ja")
-          assert_equal "第２７章", I18n.t("chapter", 27)
+        File.open(file, "w"){|f| f.write("locale: ja\npart: Part %pRW")}
+        I18n.setup("ja")
+        assert_equal "Part ０", I18n.t("part", 0)
 
-          File.open(file, "w"){|f| f.write("locale: ja\npart: Part %pRW")}
-          I18n.setup("ja")
-          assert_equal "Part ０", I18n.t("part", 0)
-
-          File.open(file, "w"){|f| f.write("locale: ja\npart: 第%pJ部")}
-          I18n.setup("ja")
-          assert_equal "第一部", I18n.t("part", 1)
-        end
+        File.open(file, "w"){|f| f.write("locale: ja\npart: 第%pJ部")}
+        I18n.setup("ja")
+        assert_equal "第一部", I18n.t("part", 1)
       end
     end
   end
