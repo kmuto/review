7b543e8bf44e11e7e853807934fa90aca8a75238
Masahiro Wakame <vvakame+dev@gmail.com>
2016-05-03 13:56:16 +0900

    add inline_comment to TOPBuilder (#625)
    return "" when draft is not specified

--- test/test_topbuilder.rb
+++ test/test_topbuilder.rb
@@ -134,6 +134,11 @@ class TOPBuidlerTest < Test::Unit::TestCase
     assert_equal %Q|test ① test2|, actual
   end
 
+  def test_inline_comment
+    actual = compile_inline("test @<comment>{コメント} test2")
+    assert_equal %Q|test  test2|, actual
+  end
+
   def test_inline_in_table
     actual = compile_block("//table{\n★1☆\t▲2☆\n------------\n★3☆\t▲4☆<>&\n//}\n")
     assert_equal %Q|★★1☆☆\t★▲2☆☆\n★3☆\t▲4☆<>&\n◆→終了:表←◆\n\n|, actual
