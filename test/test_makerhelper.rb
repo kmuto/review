eb9e49850051c84433fca4628269cabe9ae86ca4
Kenshi Muto <kmuto@debian.org>
2016-12-31 12:23:24 +0900

    skip test_copy_images_to_dir_convert when convert command is missing. ref #712

--- test/test_makerhelper.rb
+++ test/test_makerhelper.rb
@@ -38,7 +38,7 @@ class MakerHelperTest < Test::Unit::TestCase
   end
 
   def test_copy_images_to_dir_convert
-    if /mswin|mingw|cygwin/ !~ RUBY_PLATFORM
+    if /mswin|mingw|cygwin/ !~ RUBY_PLATFORM && system("convert -version")
       touch_file("#{@tmpdir1}/foo.eps")
 
       image_files = MakerHelper.copy_images_to_dir(@tmpdir1, @tmpdir2,
