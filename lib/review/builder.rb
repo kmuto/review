d5504d4efa61e4a4bea5e72ddb1f37ac3f36a216
Kenshi Muto <kmuto@debian.org>
2017-12-18 16:19:47 +0900

    remove //inline due to it confuses users. drop LF from loaded strings on @<inline>.

--- lib/review/builder.rb
+++ lib/review/builder.rb
@@ -428,11 +428,7 @@ module ReVIEW
     end
 
     def inline_include(file_name)
-      compile_inline File.read(file_name)
-    end
-
-    def include(file_name)
-      File.foreach(file_name) { |line| paragraph([line]) }
+      compile_inline File.read(file_name, mode: 'rt:BOM:utf-8').chomp
     end
 
     def ul_item_begin(lines)
