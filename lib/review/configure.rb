0ee20f3f5d82333df91b478dccf414d37eccdc27
Kenshi Muto <kmuto@debian.org>
2018-02-06 22:42:56 +0900

    specify -z option to make it easier to change.

--- lib/review/configure.rb
+++ lib/review/configure.rb
@@ -69,7 +69,7 @@ module ReVIEW
         'texcommand' => 'uplatex',
         'texdocumentclass' => ['jsbook', 'uplatex,oneside'],
         'dvicommand' => 'dvipdfmx',
-        'dvioptions' => '-d 5',
+        'dvioptions' => '-d 5 -z 9',
         # for PDFMaker
         'pdfmaker' => {
           'makeindex' => nil, # Make index page
