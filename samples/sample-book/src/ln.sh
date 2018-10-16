95c77456f00da52f727c2c8ebd44bd3c9902133a
Kenshi Muto <kmuto@debian.org>
2018-10-16 23:27:19 +0900

    symlink tool for sample-book

---
+++ samples/sample-book/src/ln.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd sty
+for f in ../../../../templates/latex/review-jsbook/*.cls \
+         ../../../../templates/latex/review-jsbook/review-*.sty; do
+  ln -sf $f
+done
+ln -sf ../../../../vendor/gentombow/gentombow.sty gentombow09j.sty
