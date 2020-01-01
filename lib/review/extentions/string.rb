cd6b58584e90bd4135abe9fb676415728c5e7f5a
Masayoshi Takahashi <takahashim@users.noreply.github.com>
2020-01-01 18:48:09 +0900

    remove String#each (#1459)
    `src` in `FooIndex.parse(src)` should be `Enumerable` in compilable.rb.

--- lib/review/extentions/string.rb
+++ lib/review/extentions/string.rb
@@ -2,7 +2,3 @@ if defined?(Encoding) && Encoding.respond_to?('default_external') &&
    Encoding.default_external != Encoding::UTF_8
   Encoding.default_external = 'UTF-8'
 end
-
-class String
-  alias_method :each, :each_line
-end
