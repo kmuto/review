88a01bdea2d6e48571546aad1d9cf4abfb0ca628
takahashim <takahashimm@gmail.com>
2021-01-25 01:54:08 +0900

    add a test for TOCPrinter#execute

--- test/test_tocprinter.rb
+++ test/test_tocprinter.rb
@@ -25,4 +25,22 @@ class TOCPrinterTest < Test::Unit::TestCase
     size = @toc_printer.calc_linesize("あ い\nうえ\nおAB\nCD\n")
     assert_equal 9.5, size
   end
+
+  def test_execute_syntax_book_detail
+    Dir.chdir('./samples/syntax-book') do
+      stdout = $stdout
+      tmp_io = StringIO.new
+      $stdout = tmp_io
+      begin
+        @toc_printer.execute('-d')
+        tmp_io.rewind
+        result = tmp_io.read
+        path = File.join(assets_dir, 'syntax_book_index_detail.txt')
+        expected = File.read(path)
+        assert_equal expected, result
+      ensure
+        $stdout = stdout
+      end
+    end
+  end
 end
