79375e3d4f481ac70ac4a0e2fd793f8792d74094
Kenshi Muto <kmuto@debian.org>
2022-02-05 12:17:34 +0900

    textmaker: make the output of //indepimage similar to that of //image. Closes: #1790

--- lib/review/topbuilder.rb
+++ lib/review/topbuilder.rb
@@ -519,26 +519,36 @@ module ReVIEW
       ), __FILE__, __LINE__ - 11
     end
 
-    def indepimage(_lines, id, caption = nil, metric = nil)
+    def indepimage(lines, id, caption = nil, metric = nil)
       metrics = parse_metric('top', metric)
       metrics = " #{metrics}" if metrics.present?
       blank
+      puts "◆→開始:#{@titles['image']}←◆"
       if caption_top?('image') && caption.present?
-        puts "図　#{compile_inline(caption)}"
+        indepimage_header(id, caption)
+        blank
       end
       begin
-        puts "◆→画像 #{@chapter.image(id).path.sub(%r{\A\./}, '')}#{metrics}←◆"
+        puts "◆→#{@chapter.image(id).path}#{metrics}←◆"
       rescue StandardError
         warn "image not bound: #{id}", location: location
-        puts "◆→画像 #{id}←◆"
+        lines.each do |line|
+          puts line
+        end
       end
       if !caption_top?('image') && caption.present?
-        puts "図　#{compile_inline(caption)}"
+        blank
+        indepimage_header(id, caption)
       end
+      puts "◆→終了:#{@titles['image']}←◆"
       blank
     end
 
     alias_method :numberlessimage, :indepimage
+
+    def indepimage_header(_id, caption)
+      puts "図#{I18n.t('caption_prefix_idgxml')}#{compile_inline(caption)}"
+    end
 
     def inline_code(str)
       "△#{str}☆"
