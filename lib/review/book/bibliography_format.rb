433b22fce2dcef9f997437a44b1721175f176e3b
Masanori Kado <kdmsnr@gmail.com>
2022-10-14 13:31:33 +0900

    Rubocop

--- lib/review/book/bibliography_format.rb
+++ lib/review/book/bibliography_format.rb
@@ -9,16 +9,16 @@ end
 module CiteProc
   module Ruby
     module Formats
-      class Html
-      end
+      # class Html
+      # end
 
       class Latex
         def bibliography(bibliography)
-          bibliography.header = "\\begin{description}"
-          bibliography.footer = "\\end{description}"
+          bibliography.header = '\\begin{description}'
+          bibliography.footer = '\\end{description}'
 
-          bibliography.prefix = "\\item[] "
-          bibliography.suffix = ""
+          bibliography.prefix = '\\item[] '
+          bibliography.suffix = ''
 
           bibliography.connector = "\n"
           bibliography
